b717a8d2fd203e36c29f150356219806d54ec82c
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-08-20 12:50:04 +0100

    Add logic so that nav items are updated to be active when the correct page is being shown and each click shows the correct page and hides the others

--- libs/script.js
+++ libs/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+  $('#home').show();
+  $('#skills').hide();
+  $('#project').hide();
+  $('#contact').hide();
+});
+
 // TOGGLE HAMBURGER MENU AND NAV ITEMS DISPLAY
 const menuBtn = $('.menu-btn');
 const navLinks = $('.nav-links');
@@ -12,4 +19,51 @@ menuBtn.on('click', () => {
     navLinks.removeClass('show');
     menuOpen = false;
   }
+});
+
+// SINGLE PAGE NAVIGATION
+const homeLink = $('.home-link');
+const skillsLink = $('.skills-link');
+const projectsLink = $('.projects-link');
+const contactLink = $('.contact-link');
+
+homeLink.on('click', () => {
+  homeLink.addClass('link-active');
+  skillsLink.removeClass('link-active');
+  projectsLink.removeClass('link-active');
+  contactLink.removeClass('link-active');
+  $('#home').show();
+  $('#skills').hide();
+  $('#projects').hide();
+  $('#contact').hide();
+});
+skillsLink.on('click', () => {
+  homeLink.removeClass('link-active');
+  skillsLink.addClass('link-active');
+  projectsLink.removeClass('link-active');
+  contactLink.removeClass('link-active');
+  $('#home').hide();
+  $('#skills').show();
+  $('#projects').hide();
+  $('#contact').hide();
+});
+projectsLink.on('click', () => {
+  homeLink.removeClass('link-active');
+  skillsLink.removeClass('link-active');
+  projectsLink.addClass('link-active');
+  contactLink.removeClass('link-active');
+  $('#home').hide();
+  $('#skills').hide();
+  $('#projects').show();
+  $('#contact').hide();
+});
+contactLink.on('click', () => {
+  homeLink.removeClass('link-active');
+  skillsLink.removeClass('link-active');
+  projectsLink.removeClass('link-active');
+  contactLink.addClass('link-active');
+  $('#home').hide();
+  $('#skills').hide();
+  $('#projects').hide();
+  $('#contact').show();
 });
