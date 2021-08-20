447121f8d3587555f0c818a555dc2b688cd79a7b
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-08-20 12:12:44 +0100

    Add animation to hide and show nav links on click

--- libs/script.js
+++ libs/script.js
@@ -1,13 +1,15 @@
-// TOGGLE HAMBURGER MENU
-
+// TOGGLE HAMBURGER MENU AND NAV ITEMS DISPLAY
 const menuBtn = $('.menu-btn');
+const navLinks = $('.nav-links');
 let menuOpen = false;
 menuBtn.on('click', () => {
   if (!menuOpen) {
     menuBtn.addClass('open');
+    navLinks.addClass('show');
     menuOpen = true;
   } else {
     menuBtn.removeClass('open');
+    navLinks.removeClass('show');
     menuOpen = false;
   }
 });
